46cc1c42945cc3850b3aa4b058a5c01002f1fdd4
stellaholbrook <jholbroo@protonmail.com>
2025-03-25 18:27:35 -0400

    fxn prototypes and input validation

--- sqlite3_geohash.cc
+++ sqlite3_geohash.cc
@@ -21,16 +21,31 @@
 #include <string.h>
 #include <math.h>
 #include "sqlite3_geohash.h"
+#include "geohash.h"
 
-static void geohash_encode(	sqlite3_context *context, int argc, sqlite3_value **argv){
+void validate_Lat_Lon(sqlite3_context *context, double lat, double lon){
+	if (a == SQLITE_NULL || b == SQLITE_NULL) {
+    sqlite3_result_error(context, "Null types.", -1);
+  }
+  if (a != SQLITE_FLOAT || a != b) {
+    sqlite3_result_error(context, "Incompatible types.", -1);
+  }
+} 
 
+void geohash_encode(	sqlite3_context *context, int argc, sqlite3_value **argv){
 	// first: check how geohash.h wants to get lat,lon pairs
+	GeoHash::Point *p
+	double lat, lon
+	p = sqlite3_aggregate_context(context, sizeof(*p))
+	lat = sqlite3_value_numeric_type(argv[0]);
+  	lon = sqlite3_value_numeric_type(argv[1]);
+
 }
 
-static void Geohash_decode( sqlite3_context *context, int argc, sqlite3_value **argv){
+void Geohash_decode( sqlite3_context *context, int argc, sqlite3_value **argv){
 
 }
 
-static void geohash_decode_rectangle( sqlite3_context *context, int argc, sqlite3_value **argv){
+void geohash_decode_rectangle( sqlite3_context *context, int argc, sqlite3_value **argv){
   
 }
