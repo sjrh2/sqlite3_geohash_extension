d92f2cfadb92bf9d4fb379ec90bd430f93330225
stellaholbrook <jholbroo@protonmail.com>
2025-03-25 17:34:08 -0400

    actually add main.cc for testing

--- main.cc
+++ main.cc
@@ -1,16 +1,17 @@
 #include "geohash.h"
-
 #include <iostream>
 #include <utility>
+#include <vector>
+#include <ostream>
+#include <sstream>
+
 
 struct Place{
 	GeoHash::Point		point;
-	std::string_view	name;
-	double			supposed_distance;
-
+	std::string      	name;
+	double			    supposed_distance;
 	GeoHash::buffer_t	buffer;
-
-	std::string_view	hash = GeoHash::encode(point, 12, buffer);
+	std::string     	hash = GeoHash::encode(point, 12, buffer);
 };
 
 Place const places[] {
@@ -33,9 +34,50 @@ Place const places[] {
 	{ { 42.68342508736217, 23.31633975360111 }, "Chevermeto"		, 1200 }
 };
 
+GeoHash::Point const test_points[] {
+	{ 44.968046,   -94.420307  },
+	{ 44.33328,    -89.132008  },
+	{ 33.755787,   -116.359998 },
+	{ 33.844843,   -116.54911  },
+	{ 44.92057,    -93.44786   },
+	{ 44.240309,   -91.493619  },
+	{ 44.968041,   -94.419696  },
+	{ 44.333304,   -89.132027  },
+	{ 33.755783,   -116.360066 },
+	{ 33.844847,   -116.549069 },
+	{ 44.920474,   -93.447851  },
+	{ 44.240304,   -91.493768  }
+};
+
 constexpr GeoHash::Point me{ 42.69207945916779, 23.325029867108032 };
 
 namespace{
+
+	std::ostream& operator<<(std::ostream& os, GeoHash::Point& arg)
+    {
+        os << arg.lat << " " << arg.lon;
+        return os;
+    }
+
+    std::ostream& operator<<(std::ostream& os, GeoHash::Rectangle& arg)
+    {
+    	os << arg.sw.lat << " " << arg.sw.lon << " | " << arg.ne.lat << " " << arg.ne.lon;
+        return os;
+    }
+
+    std::string to_string(GeoHash::Point& arg)
+    {
+        std::ostringstream ss;
+        ss << arg;
+        return std::move(ss).str();
+    }
+
+    std::string to_string(GeoHash::Rectangle& arg)
+    {
+        std::ostringstream ss;
+        ss << arg;
+        return std::move(ss).str();
+    }
 
 	void mySearchNearby(GeoHash::Point me, double radius, GeoHash::GeoSphere const &sphere){
 		auto const cells = GeoHash::nearbyCells(me, radius, sphere);
@@ -58,6 +100,38 @@ namespace{
 		}
 	}
 
+	void myEncodeDecode(){
+		GeoHash::buffer_t  buffer;
+		std::vector<GeoHash::Rectangle> rectangles = {};
+		std::vector<std::string> geohashes = {};
+		std::vector<GeoHash::Point> lat_lon = {};
+
+		for(auto &coord : test_points){
+			lat_lon.push_back(coord);
+			std::string geohash_addr = GeoHash::encode(coord, 20, buffer);
+			std::cout << geohash_addr << '\n';
+			geohashes.push_back(geohash_addr);
+		}
+		std::cout << '\n' << "test if appears correctly in a vector" << '\n';
+		for (int i = 0; i < geohashes.size(); i++) {
+        std::cout << geohashes[i] << " " << '\n';
+    	}
+    	std::cout << '\n' << "encode these back into rectangles, print out mbr points" << '\n';
+    	for (int i = 0; i < geohashes.size(); i++) {
+        rectangles.push_back(GeoHash::decode(geohashes[i]));
+    	}
+    	for (int i = 0; i < geohashes.size(); i++) {
+        std::cout << to_string(rectangles[i].sw) << " "  << to_string(rectangles[i].ne) << '\n';
+    	}
+    	std::cout << '\n' << "test centroid method" << '\n';
+    	lat_lon.clear();
+    	for (int i = 0; i < geohashes.size(); i++) {
+        lat_lon.push_back(GeoHash::decode_centroid(geohashes[i]));
+    	}
+    	for (int i = 0; i < geohashes.size(); i++) {
+        std::cout << to_string(rectangles[i]) << " " << '\n';
+    	}
+	}
 }
 
 // if for some strange reason we want distances in decameters...
@@ -73,6 +147,9 @@ int main(){
 	// 12 m - 3x2
 	auto const radius = 80;
 
-	mySearchNearby(me, radius, UNITS);
-}
+	//mySearchNearby(me, radius, UNITS);
 
+	std::cout << "\n" << "Testing my own Encode and Decode:" << "\n";
+
+	myEncodeDecode();
+}
